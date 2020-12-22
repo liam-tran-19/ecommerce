35f427f4515a7ff309eba3f836e2d032890feb3d
son-tran-97 <hongsoncht@gmail.com>
2020-12-22 15:38:00 -0500

    change route

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const mongoose = require("mongoose");
 const productRoute = require("./routes/productRoute");
 const orderRoute = require("./routes/orderRoute");
 const userRoute = require("./routes/userRoute");
-const cors = require("cors");
 
 const app = express();
 
@@ -13,39 +12,10 @@ const path = require("path");
 
 app.use(bodyParser.json());
 
-// app.use("/", express.static(__dirname));
-// app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
-
-if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
-  });
-}
-
-// ** MIDDLEWARE ** //
-const whitelist = [
-  "http://localhost:3000",
-  "http://localhost:5000",
-  "https://git.heroku.com/whispering-sierra-93484.git",
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin);
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable");
-      callback(null, true);
-    } else {
-      console.log("Origin rejected");
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 
 app.use("/ecommerce/products", productRoute);
 app.use("/ecommerce/orders", orderRoute);
